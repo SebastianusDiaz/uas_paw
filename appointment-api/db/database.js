c777ce1845faf92948d33d8709b4a4f30782b576
SebastianusDiaz <sebastianusdiaz@gmail.com>
2025-01-12 22:12:33 +0700

    Update database.js

--- appointment-api/db/database.js
+++ appointment-api/db/database.js
@@ -2,6 +2,6 @@
 //   db: "mongodb://127.0.0.1:27017/restapi",
 // };
 module.exports = {
-  db: "mongodb+srv://ma:poop123@cluster0.jy0at.mongodb.net/",
+  db: "mongodb+srv://sebastianusdiaz:poop123@cluster0.rbx7x.mongodb.net/",
 };
  
